2fb5a4107378e1a5e07efd5e43e79a6941a151a5
Punpun <suracha2550@gmail.com>
2025-05-02 23:55:18 +0700

    Configuring

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -3,6 +3,9 @@ return {
   event = "VeryLazy",
   opts = {
     -- add any options here
+    presets = {
+      lsp_doc_border = true
+    }
   },
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
@@ -11,5 +14,5 @@ return {
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
     { "rcarriga/nvim-notify", opts = { background_colour = "#000000" } }
-    }
+  }
 }
