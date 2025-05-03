caac71d4cf24762d257b579c0f9d8ed164827c3e
Punpun <suracha2550@gmail.com>
2025-05-03 14:17:17 +0700

    Change lualine section

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -12,7 +12,7 @@ return {
             sections = {
                 lualine_a = {{ 'mode', icon='󰗶' }},
                 lualine_b = {'branch', 'diff', 'diagnostics'},
-                lualine_c = {'filename'},
+                lualine_c = {'buffers'},
                 lualine_x = {'encoding', 'fileformat', 'filetype'},
                 lualine_y = {'progress'},
                 lualine_z = {'location'}
